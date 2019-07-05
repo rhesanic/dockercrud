e7f21dd34fc391d059b85a546da997a58be7eaf4
rhesanic <rhesanicop@gmail.com>
2019-07-05 14:48:45 +0700

    cekcekekeck

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM php:5.6.11-apache 
+FROM php:7.1.2-apache 
 RUN docker-php-ext-install mysqli
--- dump/myDb.sql
+++ dump/myDb.sql
@@ -1,22 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: db
--- Generation Time: Dec 27, 2017 at 02:06 PM
--- Server version: 5.7.20
--- PHP Version: 5.6.11
+-- put in ./dump directory 
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET namaS utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `crud_db`
