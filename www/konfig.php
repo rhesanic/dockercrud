8f96bd29a53301e7032483e3039606b2460041f0
rhesanic <rhesanicop@gmail.com>
2019-07-05 11:57:16 +0700

    tugasthuastuhsathuashtu

--- www/konfig.php
+++ www/konfig.php
@@ -3,10 +3,10 @@
  * using mysqli_connect for database connection
  */
  
-$databaseHost = '172.17.0.1';
-$databaseName = 'crud_db';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = "172.17.0.1";
+$databaseName = "crud_db";
+$databaseUsername = "root";
+$databasePassword = "root";
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
