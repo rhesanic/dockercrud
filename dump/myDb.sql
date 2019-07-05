74d52707a5008a11a002566d7246bceee0850386
rhesanic <rhesanicop@gmail.com>
2019-07-05 15:02:14 +0700

    cekcekekeck

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -9,15 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `crud_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `anggota`
---
 
 CREATE TABLE `anggota` (
   `id` int(11) NOT NULL,
@@ -26,34 +17,15 @@ CREATE TABLE `anggota` (
   `alamat` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `anggota`
---
+
 
 INSERT INTO `anggota` (`id`, `nama`, `email`, `alamat`) VALUES
 (3, 'rhesa', 'rhesanicop@gmail,com', 'kalasan');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `anggota`
---
 ALTER TABLE `anggota`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `anggota`
---
 ALTER TABLE `anggota`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
